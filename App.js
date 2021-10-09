1247abef95abb47c29dc01e5022a90b230cf3161
Konlawat Hutsaithong <konlawatit@gmail.com>
2021-10-09 21:26:13 +0700

    add context

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -8,31 +8,47 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import DashboardScreen from "./screens/DashboardScreen";
 
+//Context
+import AppContext from "./components/AppContext";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [name, setName] = useState("Bas");
+  const [age, setAge] = useState();
+
+  const user = {
+    name,
+    age,
+    setName,
+    setAge,
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{
-        headerShown: false
-      }}>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Welcome" }}
-        />
-        <Stack.Screen name="Dashboard" component={DashboardScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AppContext.Provider value={{user}} >
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: "Welcome" }}
+          />
+          <Stack.Screen name="Dashboard" component={DashboardScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AppContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import AppContext from '../components/AppContext';
 
 export default function HomeScreen({navigation}) {
+  const context = useContext(AppContext)
   return (
     <View style={styles.container}>
-      <Text>HomeScreen</Text>
+      <Text>HomeScreen <Text style={{color: 'red'}} >{context.user.name}</Text></Text>
       <Button title="Go to Dashboard Screen"
       onPress={() => navigation.navigate('Dashboard', {name: 'Hi'})}
       />
+      <Button title="Chnage name" onPress={() => context.user.name !== 'Bas' ? context.user.setName('Bas') : context.user.setName('Ball')} />
     </View>
   );
 }
