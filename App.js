8f9812419e22d52bf808aa5d95227dbf6df87c57
bb0b <59884776+konlawatit@users.noreply.github.com>
2021-10-11 01:40:04 +0700

    add sqlite

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+//sqlite
+import { DatabaseConnection } from "./src/database/database-connection";
+
+const db = DatabaseConnection.getConnection();
+
 //Screens
-import HomeScreen from "./screens/HomeScreen";
-import DashboardScreen from "./screens/DashboardScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import DashboardScreen from "./src/screens/DashboardScreen";
 
 //Context
-import AppContext from "./components/AppContext";
+import AppContext from "./src/components/AppContext";
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [uid, setUID] = useState("it6207007");
   const [name, setName] = useState("Bas");
-  const [age, setAge] = useState();
+  const [age, setAge] = useState(21);
+
+  useEffect(() => {
+    db.transaction((tx) => {
+      // tx.executeSql("DROP TABLE USERS", []);
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS USERS (uid varchar(255) PRIMARY KEY, name varchar(255), age INT)`,
+        [],
+        (tx, results) => {
+          console.log("create table successfully");
+        },
+        (error) => console.log("craete table error", error)
+      );
+
+      tx.executeSql(
+        `select uid from USERS where uid='${uid}'`,
+        [],
+        (tx, results) => {
+          if (results.rows.length === 0) {
+            tx.executeSql(
+              `INSERT INTO USERS (uid, name, age) VALUES (?, ?, ?)`,
+              [uid, name, age],
+              (tx, results) => console.log("insert successfully"),
+              (error) => console.log(2, error)
+            );
+          }
+
+          tx.executeSql(
+            `SELECT * FROM USERS`,
+            [],
+            (tx, results) => console.log(results.rows),
+            (error) => console.log(error)
+          );
+        },
+        (error) => console.log(error)
+      );
+    });
+  }, []);
 
   const user = {
     name,
@@ -25,7 +69,7 @@ export default function App() {
   };
 
   return (
-    <AppContext.Provider value={{user}} >
+    <AppContext.Provider value={{ user }}>
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{
